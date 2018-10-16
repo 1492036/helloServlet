feb8f78169dc77b433b4c97bd6457a4f116c8130
J <J@DESKTOP-QME7QO8>
2018-10-16 21:26:45 +0900

    First Servlet Project Serup

---
+++ src/kr/ac/hansung/logInServlet.java
@@ -0,0 +1,35 @@
+package kr.ac.hansung;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logInServlet")
+public class logInServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		//Build HTML code
+		response.setContentType("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String htmlResponse = "<html>";
+		htmlResponse +="<h2> your name is " + name + "<br/>";
+		htmlResponse +="your password is" + password + "</h2>";
+		htmlResponse +="</html>";
+		
+		out.println(htmlResponse);
+		
+	}
+
+}
